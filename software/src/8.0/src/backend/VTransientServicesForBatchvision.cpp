3d8a98c38190cf9213fba8089a8715ffd01dc5e3
Mike <mcaruso@alum.mit.edu>
2016-12-12 13:03:34 -0500

    Option to disable batchvision's automatic memory allocation limit.
    To be a good citizen in Linux and Solaris production environments,
    batchvision has long imposed a 2gb private memory allocation limit
    on itself unless it detects that an explicit limit has already been
    set (unix/linux getrlimit RLIMIT_DATA != RLIM_INFINITY).  With this
    change, batchvision's self-limiting behavior can also be disabled by
    setting the 'VisionDataLimitDisabled' environment variable (to any
    value).

--- software/src/8.0/src/backend/VTransientServicesForBatchvision.cpp
+++ software/src/8.0/src/backend/VTransientServicesForBatchvision.cpp
@@ -61,12 +61,12 @@ VTransientServicesForBatchvision::VTransientServicesForBatchvision ()
 {
 #if defined (__linux__) || defined (sun)
 
+    // Unless directed otherwise, ...
     // Don't allow unlimited datasize. If unlimited, fall back to 2 GB ..
     // Any finite limit is acceptable.
-
     struct rlimit datasize;
     if (-1 == getrlimit (RLIMIT_DATA, &datasize) ||  RLIM_INFINITY == datasize.rlim_cur){
-	m_sMemoryAllocationLimit = 0x80000000;
+	m_sMemoryAllocationLimit = getenv ("VisionDataLimitDisabled") ? 0xffffffff00000000ull :  0x80000000;
     } else {
 	m_sMemoryAllocationLimit = datasize.rlim_cur;
     }
