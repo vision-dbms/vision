18c561b34f60e60ed93609a2ad32ba1d7ca4fdbd
Charlie Kuhlman <charles_kuhlman@troweprice.com>
2020-01-31 16:02:47 -0500

    include the namespace in the parameter declaration in getDescription so that it will link under Solaris

--- software/src/master/src/kernel/Vxa_VType.cpp
+++ software/src/master/src/kernel/Vxa_VType.cpp
@@ -58,7 +58,7 @@ Vxa::VType::~VType () {
  *************************
  *************************/
 
-V::VString &Vxa::VType::getDescription_(VString &rResult) const {
+V::VString &Vxa::VType::getDescription_(V::VString &rResult) const {
     V::VRTTI iRTTI (typeid (*this));
     return rResult << iRTTI.name ();
 }
